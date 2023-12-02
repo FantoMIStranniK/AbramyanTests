7f00da5250e6329b15a6e35860aed1766219d42e
-FantoMIStranniK- <synergyivanb@gmail.com>
2023-12-02 12:49:28 +0200

    Base added

---
+++ AbramyanTest/AbramyanTest/December.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AbramyanTest
+{
+    public class December
+    {
+        public void RunTest()
+        {
+
+        }
+    }
+}
---
+++ AbramyanTest/AbramyanTest/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AbramyanTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            December test = new December();
+
+            test.RunTest();
+        }
+    }
+}
